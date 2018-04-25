63f8c5cafea2b2a4e6036d5c614aea9214cc8117
kaushik18 <kaushik19599@gmail.com>
2018-04-24 21:04:26 -0500

    Use binary file I/O to read the header record from the binary file, and displayed header contents into first row of matrix.

--- cdkexample.cc
+++ cdkexample.cc
@@ -8,14 +8,42 @@
 
 #include <iostream>
 #include "cdk.h"
+#include <stdint.h>
+#include <fstream>
+#include <stdio.h>
 
 
 #define MATRIX_WIDTH 3
 #define MATRIX_HEIGHT 5
-#define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
+#define BOX_WIDTH 25
+#define MATRIX_NAME_STRING "Binary File Contents"
 
 using namespace std;
+
+class BinaryFileHeader
+{
+
+ public:
+
+  uint32_t magicNumber;
+  uint32_t versionNumber;
+  uint64_t numRecords;
+};
+
+/* Should be 0xFEEDFACE */
+
+
+/*
+ * Records in the file have a fixed length buffer * that will hold a C-Style string. This is the * size of the fixed length buffer.
+ */
+
+const int maxRecordStringLength = 25;
+
+class BinaryFileRecord
+ { public:
+   uint8_t strLength;
+   char stringBuffer[maxRecordStringLength];
+ };
 
 
 int main()
@@ -33,8 +61,8 @@ int main()
   // values you choose to set for MATRIX_WIDTH and MATRIX_HEIGHT
   // above.
 
-  const char 		*rowTitles[] = {"R0", "R1", "R2", "R3", "R4", "R5"};
-  const char 		*columnTitles[] = {"C0", "C1", "C2", "C3", "C4", "C5"};
+  const char 		*rowTitles[] = {"R0", "a", "b", "c", "d", "e", "f"};
+  const char 		*columnTitles[] = {"C0","a", "b", "c", "d", "e", "f"};
   int		boxWidths[] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int		boxTypes[] = {vMIXED, vMIXED, vMIXED, vMIXED,  vMIXED,  vMIXED};
 
@@ -62,14 +90,28 @@ int main()
       _exit(1);
     }
 
+  char buffr[1024];
+
+
+  BinaryFileHeader *headerRecord = new BinaryFileHeader();
+  ifstream binFileInput ("cs3377.bin", ios::in | ios::binary);
+
+  binFileInput.read((char *) headerRecord, sizeof(BinaryFileHeader));  
+   
+  sprintf(buffr, "Magic: 0x%X", headerRecord->magicNumber);
+  setCDKMatrixCell(myMatrix, 1, 1, buffr); 
+
+  sprintf(buffr, "Version: %d", headerRecord->versionNumber);
+  setCDKMatrixCell(myMatrix, 1, 2, buffr);
+  
+  sprintf(buffr, "NumRecords: %lu", headerRecord->numRecords);
+  setCDKMatrixCell(myMatrix, 1, 3, buffr);
+
+  binFileInput.close();
+
   /* Display the Matrix */
   drawCDKMatrix(myMatrix, true);
 
-  /*
-   * Dipslay a message
-   */
-  setCDKMatrixCell(myMatrix, 2, 2, "Test Message");
-  drawCDKMatrix(myMatrix, true);    /* required  */
 
   /* So we can see results, pause until a key is pressed. */
   unsigned char x;
